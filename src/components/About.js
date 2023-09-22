3a3713d12257008531ffacfec47cea27cf2954d5
samira toubal seghir <samsidt@gmail.com>
2023-09-23 01:36:48 +0200

    Complited Assignemnts

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,32 @@
 import React from "react";
-
-function About() {
+import user from "../data/user";
+import Links from "./Links";
+function About(props) {
+  const bio = props.bio;
+  if (bio==undefined) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
+      <Links github={props.linkG} linkedin={props.linkL} />
+      </div>
+  );}
+  else  if (bio=="") {
+    return (
+      <div id="about">
+        <h2>About Me</h2>
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+        <Links github={props.linkG} linkedin={props.linkL} />
+        </div>
+    );}
+    else 
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>{props.bio}</p>
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      <Links github={props.linkG} linkedin={props.linkL} />
+      </div>
   );
 }
 
